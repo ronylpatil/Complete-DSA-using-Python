11833f76c3bbf2bfc0e62a57d235be78ec0a4585
Ronil Patil <63307564+ronylpatil@users.noreply.github.com>
2023-03-08 18:25:37 +0530

    Create stack_wid_inc_operation.py

---
+++ stack_wid_inc_operation.py
@@ -0,0 +1,16 @@
+class CustomStack :
+    def __init__(self, maxSize: int) :
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None :
+        if len(self.stack) < self.maxSize : self.stack.append(x)
+
+    def pop(self) -> int :
+        if self.stack : 
+            return self.stack.pop()
+        return -1
+
+    def increment(self, k: int, val: int) -> None :
+        for i in range(min(k, len(self.stack))) : 
+            self.stack[i] = self.stack[i] + val
